830ea0c5b705ffe1c6c39bbbd646c85b00143afc
Rust | Metaplex | Terra | Blockchain + React Developer <kraiglyons0929@gmail.com>
2022-01-28 19:24:27 +0800

    payable token

---
+++ contracts/mocks/ERC1363PayableMock.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../payment/ERC1363Payable.sol";
+
+// mock class using ERC1363Payable
+contract ERC1363PayableMock is ERC1363Payable {
+    uint256 public transferNumber;
+    uint256 public approvalNumber;
+
+    constructor(IERC1363 acceptedToken) ERC1363Payable(acceptedToken) {}
+
+    function _transferReceived(
+        address, /* operator */
+        address, /* sender */
+        uint256, /* amount */
+        bytes memory /* data */
+    ) internal override {
+        transferNumber += 1;
+    }
+
+    function _approvalReceived(
+        address, /* sender */
+        uint256, /* amount */
+        bytes memory /* data */
+    ) internal override {
+        approvalNumber += 1;
+    }
+}
